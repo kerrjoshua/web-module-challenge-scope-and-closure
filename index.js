8c5b85acf631343fde72df022c9f34c2b6157fe7
Joshua Kerr <kerrjoshua@gmail.com>
2022-10-15 16:52:37 +0900

    Completed task 2.

--- index.js
+++ index.js
@@ -65,11 +65,12 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    return Math.floor(Math.random * 3);
+function inning(){
+    return Math.floor(Math.random() * 3);
 }
 
-console.log(inning());
+
+console.log('Task 2:  ', inning());
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
